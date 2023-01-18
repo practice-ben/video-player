46a939dd35e246fefa2154304b1de826c8518680
Benson Njuguna <njugunab655@gmail.com>
2023-01-18 12:43:11 +0300

    Add container to video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,5 +1,7 @@
 export default function Video({link}) {
     return (
-        <video src={link} loop controls autoStart="true" autoPlay muted />
+        <div className="video">
+            <video src={link} loop controls autoStart="true" autoPlay muted />
+        </div>
     )
 }
